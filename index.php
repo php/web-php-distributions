c80a11785458dae3dab709fa5b4812eee601e74c
Gabor Hojtsy <goba@php.net>
2003-05-03 15:59:06 +0000

    Fixing the URL shortcut simlulations

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
 // Simulate a /downloads shortcut call
+$_SERVER['REQUEST_URI'] = $HTTP_SERVER_VARS['REQUEST_URI'] = '/downloads';
 include_once 'prepend.inc';
-$REQUEST_URI = '/downloads';
 include_once "$DOCUMENT_ROOT/error/index.php";
 
 ?>
