954f5dcec5db18aafbedb4334592a1073ae1acca
Gabor Hojtsy <goba@php.net>
2003-08-09 16:51:24 +0000

    Do not require register_globals

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-
 // Simulate a /downloads shortcut call
-$_SERVER['REQUEST_URI'] = $HTTP_SERVER_VARS['REQUEST_URI'] = '/downloads';
-include_once 'prepend.inc';
-include_once "$DOCUMENT_ROOT/error/index.php";
-
+$_SERVER['REQUEST_URI'] = '/downloads';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/include/prepend.inc';
+include_once $_SERVER['DOCUMENT_ROOT'] . '/error.php';
 ?>
